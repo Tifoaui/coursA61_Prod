66a59aeba88d1d9bfc72f36ea6575deebdbf4424
Tifoaui <tifaouisaid@gmail.com>
2021-02-13 12:46:19 -0400

    Partie-13- Configuration CircleCi Integration Continue

--- packages/regression_model/setup.py
+++ packages/regression_model/setup.py
@@ -10,14 +10,10 @@ from setuptools import find_packages, setup
 
 # Package meta-data.
 NAME = 'regression_model'
-# DESCRIPTION = 'Train and deploy regression model.'
-# URL = 'your github project'
-# EMAIL = 'your_email@email.com'
-# AUTHOR = 'Your name'
-DESCRIPTION = 'Former et deployer un modele de regression.'
-URL = 'https://github.com/Tifoaui/coursA61_Prod.git'
-EMAIL = 'tifaouisaid@gmail.com'
-AUTHOR = 'Tifoaui'
+DESCRIPTION = 'Train and deploy regression model.'
+URL = 'your github project'
+EMAIL = 'your_email@email.com'
+AUTHOR = 'Your name'
 REQUIRES_PYTHON = '>=3.6.0'
 
 
